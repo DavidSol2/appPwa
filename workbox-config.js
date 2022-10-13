96a533dd9f840ac1296e6ffeda9c9d3018f859e2
DavidSol2 <dsalvadors@gmail.com>
2022-10-13 09:52:31 -0600

    Update workbox-config.js

--- workbox-config.js
+++ workbox-config.js
@@ -9,3 +9,16 @@ module.exports = {
 		/^fbclid$/
 	]
 };
+self.addEventListener('activate', function(event) {
+    event.waitUntil(caches.keys().then(function(names) {
+        return Promise.all(
+            names.filter(function(name) {
+                return name !== cacheName;
+            }).map(function(name) {
+                return caches.delete(name);
+            })
+        );
+    }).then(function() {
+        return self.clients.claim();
+    }));
+});

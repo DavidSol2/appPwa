54a4a61cd4ecea8dcb0a71b989d0700767476d7a
DavidSol2 <dsalvadors@gmail.com>
2022-10-13 18:10:16 -0600

    jk

--- workbox-config.js
+++ workbox-config.js
@@ -3,31 +3,9 @@ module.exports = {
 	globPatterns: [
 		'**/*.{css,jpg,html,js,json}'
 	],
-	runtimeCaching: [
-        {
-          urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
-          handler: 'CacheFirst',
-          options: {
-            cacheName: 'images',
-            expiration: {
-              maxEntries: 10,
-              maxAgeSeconds: 60 * 1, // 30 Days
-            },
-          },
-        },
-        {
-          urlPattern: /\.(?:css|js)$/,
-          handler: 'StaleWhileRevalidate',
-          options: {
-            cacheName: 'assets',
-          },
-        }
-    ],  
-    swDest: 'sw.js',
+	swDest: 'sw.js',
 	ignoreURLParametersMatching: [
 		/^utm_/,
 		/^fbclid$/
-	],
-
+	]
 };
-
